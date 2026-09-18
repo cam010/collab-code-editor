c0e68f0c54fca44b057a53a5f7945c051deadc19
Cam <cameronteclarke@gmail.com>
2026-09-18 17:36:43 +0100

    Created first express server

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
